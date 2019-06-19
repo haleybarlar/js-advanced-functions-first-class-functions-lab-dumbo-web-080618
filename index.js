22a3d3501cf6fb0f68bed462bec9074b8efb29ff
Haley Barlar <hbarlar1@gmail.com>
2019-06-19 08:48:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 const returnFirstTwoDrivers = function() {
   return drivers.slice(0,2)
 }
+
+const returnLastTwoDrivers = function() {
+  return drivers.slice(-2)
+}
